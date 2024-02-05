51c8f51224363227ffee172936bc606e35562e57
gato <dshirnin9130@gmail.com>
2024-02-06 00:34:37 +0300

    bot added

--- config.py
+++ config.py
@@ -5,4 +5,8 @@ load_dotenv()
 
 API_ID = os.getenv('API_ID')
 API_HASH = str(os.getenv('API_HASH'))
+<<<<<<< HEAD
 BOT_TOKEN = str(os.getenv('BOT_TOKEN'))
+=======
+BOT_TOKEN = str(os.getenv('BOT_TOKEN'))
+>>>>>>> efe0fd5 (bot added)
