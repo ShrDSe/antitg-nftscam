c90563518a37947e2812952ce5f64e5dd8978c90
gato <dshirnin9130@gmail.com>
2024-02-06 00:54:21 +0300

    huh

---
+++ bot.py
@@ -0,0 +1,27 @@
+<<<<<<< HEAD
+=======
+import re, requests
+from pyrogram import Client, filters
+from config import API_ID, API_HASH, BOT_TOKEN
+
+bot = Client('anti_shit', 
+            api_id=API_ID, 
+            api_hash=API_HASH, 
+            bot_token=BOT_TOKEN)
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+
+
+@bot.on_message(filters.text)
+async def check_link(client, message):
+    links = re.findall(r'(https?://\S+)', message.text)
+    if links:
+        print(links)
+        for link in links:
+            if requests.get(link + 'images/hero.png', headers=headers).status_code == 200:
+                await message.delete()
+                print("deleted " + link)
+                break
+
+bot.run()
+>>>>>>> a8040c1 (huh)
--- config.py
+++ config.py
@@ -6,7 +6,11 @@ load_dotenv()
 API_ID = os.getenv('API_ID')
 API_HASH = str(os.getenv('API_HASH'))
 <<<<<<< HEAD
+<<<<<<< HEAD
 BOT_TOKEN = str(os.getenv('BOT_TOKEN'))
 =======
 BOT_TOKEN = str(os.getenv('BOT_TOKEN'))
 >>>>>>> efe0fd5 (bot added)
+=======
+BOT_TOKEN = str(os.getenv('BOT_TOKEN'))
+>>>>>>> a8040c1 (huh)
